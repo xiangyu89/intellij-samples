a5ee897fa4ebd93f8545278b569c80f25eb29c19
Trisha Gee <trisha.gee@gmail.com>
2020-08-06 13:57:02 +0200

    Starting to flesh out feature 123

--- standard-java/src/main/java/com/jetbrains/completion/LiveTemplates.java
+++ standard-java/src/main/java/com/jetbrains/completion/LiveTemplates.java
@@ -4,13 +4,16 @@ import com.jetbrains.inspections.entities.Person;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @SuppressWarnings("unused")
 public class LiveTemplates {
 
     public List<Person> getEmployed(final List<Person> people) {
         List<Person> employees = new ArrayList<>();
+
+        for (Person employee : employees) {
+            System.out.println("employee = " + employee);
+        }
 
         return employees;
     }
